290829337e5be139a0e0028eacadf8fb43a87722
Aidan Lynch <aidantlynch00@gmail.com>
2019-05-29 23:07:29 -0500

    Error correction

--- camera.py
+++ camera.py
@@ -4,10 +4,11 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 
-def variance(frame1, frame2):
-    img_width, img_height = frame1.size()
+def compute_variance(frame1, frame2):
+    img_width, img_height = frame1.size
+    print 'Width: ', img_width, '   Height: ', img_height	
     
-    variance_sums = [3]
+    variance_sums = [0] * 3
 
     #For each pixel
     for x in range(img_width):
@@ -16,10 +17,13 @@ def variance(frame1, frame2):
             rgb1 = frame1.getpixel((x, y))
             rgb2 = frame2.getpixel((x, y))
 
+	    print(rgb1)
+	    print(rgb2)
+
             #Difference in each channel represented as a number between 0 and 1
-            r_variance = abs(rgb1[0] - rgb2[0]) / 255
-            g_variance = abs(rgb1[1] - rgb2[1]) / 255
-            b_variance = abs(rgb1[2] - rgb2[2]) / 255
+            r_variance = float(abs(rgb1[0] - rgb2[0])) / 255
+            g_variance = float(abs(rgb1[1] - rgb2[1])) / 255
+            b_variance = float(abs(rgb1[2] - rgb2[2])) / 255
 
             #Add to the overall variance in the image
             variance_sums[0] += r_variance
@@ -45,31 +49,34 @@ camera.led = False
 
 #Declare bit stream and image variables
 stream = BytesIO()
-prev_frame = None
-curr_frame = None
+prev_frame = Image.new('RGB', (320, 240))
+curr_frame = Image.new('RGB', (320, 240))
 
 #Assign variance threshold
 threshold = .13 #TODO: change arbitrary number lol
 
+sleep(2)
+
 #Main camera loop
 while True:
-    prev_frame = curr_frame
+    print 'Loop'
+    prev_frame = curr_frame.copy()
 
     #Capture image to bit stream
     camera.capture(stream, format = 'jpeg')
     stream.seek(0) #Reset pointer to start of stream to read data
 
-    curr_frame = Image.open(stream)
-
+    curr_frame = Image.open(stream).resize((320, 240)).convert('RGB')
+    stream.flush()
+    
     #Compute variance between frames
     if prev_frame != None and curr_frame != None:
-        variance = variance(prev_frame, curr_frame)
-        print('Variance: ' + variance)
+        variance = compute_variance(prev_frame, curr_frame)
+        print 'Variance: ', variance
 
         #If the variance is greater than set threshold, pass curr_frame into OpenCV
         if variance > threshold:
             #Run OpenCV algorithm
             pass
-    sleep(5)
 
     
