c18102aa8204bfcb69d8bd8159ee5d29f0822819
Aidan Lynch <aidantlynch00@gmail.com>
2019-05-30 15:13:39 -0500

    Added folder to stored saved images that exceed threshold

--- camera.py
+++ camera.py
@@ -3,9 +3,14 @@ from time import sleep
 from PIL import Image
 from io import BytesIO
 import numpy as np
+import datetime
+import pytz
+from pytz import timezone
 
-THRESHOLD = .3
+THRESHOLD = .20 #20% of pixels have changed
 RGB_VARIANCE_THRESHOLD = 7
+
+TIMEZONE = timezone('US/Eastern')
 
 def compute_variance(frame1, frame2):
     img_width, img_height = frame1.size
@@ -21,24 +26,6 @@ def compute_variance(frame1, frame2):
 
     for x in range(img_width):
         for y in range(img_height):
-            ### SOLUTION USING PILs BUILT IN 'getpixel' METHOD ###
-            '''
-            #Fetch the RGB values of each image at the pixel (x, y)
-            rgb1 = frame1.getpixel((x, y))
-            rgb2 = frame2.getpixel((x, y))
-
-            #Difference in each channel represented as a number between 0 and 1
-            r_changed = 1 if abs(rgb1[0] - rgb2[0]) > RGB_VARIANCE_THRESHOLD else 0
-            g_changed = 1 if abs(rgb1[1] - rgb2[1]) > RGB_VARIANCE_THRESHOLD else 0
-            b_changed = 1 if abs(rgb1[2] - rgb2[2]) > RGB_VARIANCE_THRESHOLD else 0
-
-            #Add to the overall variance in the image
-            variance_sums[0] += r_changed
-            variance_sums[1] += g_changed
-            variance_sums[2] += b_changed
-            '''
-            #######################################################
-
             for channel in range(3):
                 variance_sums[channel] += 1 if abs(int(rgb1[y, x, channel]) - int(rgb2[y, x, channel])) > RGB_VARIANCE_THRESHOLD else 0
 
@@ -89,10 +76,29 @@ while True:
         #If the variance is greater than set threshold, pass curr_frame into OpenCV
         if variance > THRESHOLD:
             #Run OpenCV algorithm
+            filename = str(datetime.datetime.now(TIMEZONE))[0:-9] + '.jpg'
+            curr_frame.save('./Stored Images/')
             pass
 
 #Free camera resources
 camera.stop_preview()
 camera.close()
 
-    
+''' THIS IS THE CODE GRAVEYARD
+
+### SOLUTION USING PILs BUILT IN 'getpixel' METHOD ###
+            #Fetch the RGB values of each image at the pixel (x, y)
+            rgb1 = frame1.getpixel((x, y))
+            rgb2 = frame2.getpixel((x, y))
+
+            #Difference in each channel represented as a number between 0 and 1
+            r_changed = 1 if abs(rgb1[0] - rgb2[0]) > RGB_VARIANCE_THRESHOLD else 0
+            g_changed = 1 if abs(rgb1[1] - rgb2[1]) > RGB_VARIANCE_THRESHOLD else 0
+            b_changed = 1 if abs(rgb1[2] - rgb2[2]) > RGB_VARIANCE_THRESHOLD else 0
+
+            #Add to the overall variance in the image
+            variance_sums[0] += r_changed
+            variance_sums[1] += g_changed
+            variance_sums[2] += b_changed
+#######################################################
+'''
